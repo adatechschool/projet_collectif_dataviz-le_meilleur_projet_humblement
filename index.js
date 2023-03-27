d8f3bed3cbb9a9d08fcbdf2f48fe1b79a171bd6b
Thomassaiag <thomas.saiag@gmail.com>
2023-03-27 15:32:30 +0200

    dernieres version des filtres avant création de la branche

--- index.js
+++ index.js
@@ -1,62 +1,48 @@
 
-//récupération de l'urm
+//récupération de l'url
 const urlTournages = "https://opendata.paris.fr/api/records/1.0/search/?dataset=lieux-de-tournage-a-paris&q=&rows=10000&facet=annee_tournage&facet=type_tournage&facet=nom_tournage&facet=nom_realisateur&facet=nom_producteur&facet=ardt_lieu&facet=date_debut&facet=date_fin";
-let reponseTournages = await fetch(urlTournages) //stockage du fichier brut dans une variable reponse
-let jsonObjectTournage= await reponseTournages.json() // conversion de reponse au format Json
+//stockage du fichier brut dans une variable reponse
+let reponseTournages = await fetch(urlTournages) 
+// conversion de reponse au format Json
+let jsonObjectTournage= await reponseTournages.json()
+//stockage du tableau records dans une variable elementsTournages
+let elementsTournages = jsonObjectTournage.records 
 
-let elementsTournages = jsonObjectTournage.records //stockage du tableau records dans une variable elementsTournages
+// console.log(elementsTournages)
 
-console.log(elementsTournages)
 
+//affichage titre page HTML, à enlever
 const titreListeTypeTournage = document.getElementById("titre") 
 titreListeTypeTournage.innerText = "Voici la liste des type de tournage :"
 
+//variable pour manipuler un tableau de taille 100
 let tailleTableau = 100
-console.log(tailleTableau) // stockage de la taille du tableau dans une variable utilisée pour définir la taille de la boucle
-for (let i=0; i < tailleTableau; i++){
-    // console.log('boucle n°'+i)
-    let elementsParTournages = elementsTournages[i]
-    let typeTournage = elementsParTournages.fields.type_tournage
-    console.log(typeTournage) // récupération de la valeur de la clé type de tournage, stockée dans la clé field
-    
-    const listeTypeTournage = document.createElement('p');
-    listeTypeTournage.innerText = typeTournage
-    titreListeTypeTournage.appendChild(listeTypeTournage)
-}
 
-let filtreArr = document.getElementById("arrondissementSelect").value  
-//const elementsTournagesFiltre = elementsTournages.filter(tournage => tournage.fields.ardt_lieu == filtreArr);
- 
-  
-	const elementsTournagesFiltre = elementsTournages.filter (item => {
-    return item.fields.ardt_lieu == 75018
-  });
-  console.log(elementsTournagesFiltre)
+let elementsTournagesFiltre={}
+const boutonArr = document.getElementById("boutonArr")
+boutonArr.addEventListener("click",function(){
+  let filtreArr = document.getElementById("arrondissementSelect").value  
+  //tableau filtré par un arrondissemnt 
+   elementsTournagesFiltre = elementsTournages.filter (item => {
+      return item.fields.ardt_lieu == filtreArr
+      
+    });
+    let compteTournages= {}
 
-// const liste = [1, 30, 2, 7, 11, 25];
-// let result = filtre(elementsTournages)
-// console.log(result);
-
-
-
-//compte des tournages par année
-let compteTournages= {}
-
-console.log(compteTournages)
 
 for (let i=0;i<tailleTableau;i++){
-    let annee=elementsTournagesFiltre[i].fields.annee_tournage
-    console.log(annee)
-    if (annee in compteTournages!=true){
-      compteTournages[annee]=1
-    }
-    else {
-      compteTournages[annee]+=1
-    }
-    // compteTournages[annee]+=1
-
+  let annee=elementsTournagesFiltre[i].fields.annee_tournage
+  //console.log(annee)
+  if (annee in compteTournages!=true){
+    compteTournages[annee]=1
+  }
+  else {
+    compteTournages[annee]+=1
+  }
 }
-console.log(compteTournages)
+//console.log(compteTournages)
+
+
 
 //graphe tournages par années
 new Chart(
@@ -74,7 +60,47 @@ new Chart(
     }
   }
 );
+})
+// récuperation de la valeur dans le filtre déroulant, dans le html
 
+console.log(elementsTournagesFiltre)
+
+/*
+//compte des tournages par année
+let compteTournages= {}
+
+
+for (let i=0;i<tailleTableau;i++){
+  let annee=elementsTournagesFiltre[i].fields.annee_tournage
+  //console.log(annee)
+  if (annee in compteTournages!=true){
+    compteTournages[annee]=1
+  }
+  else {
+    compteTournages[annee]+=1
+  }
+}
+//console.log(compteTournages)
+
+
+
+//graphe tournages par années
+new Chart(
+  document.getElementById('consolidations'),
+  {
+    type: 'bar',
+    data: {
+      labels: Object.keys(compteTournages),
+      datasets: [
+        {
+          label: 'Nombre de lieux de tournage par année',
+          data: Object.values(compteTournages),
+        }
+      ]
+    }
+  }
+);*/
+// compte des arrondissement par tournages 
 let compteTournagesArr={}
 for (let i=0;i<tailleTableau;i++){
   let arrondissement=elementsTournages[i].fields.ardt_lieu
@@ -120,19 +146,22 @@ new Chart(
 
 
 
-// function lessThan10Filter(arr){
-//   arr.filter(item => item < 10);
-//   return lessThan10Filter
-// }
-
-
-// const liste = [1, 30, 2, 7, 11, 25];
-// let result = filtre(elementsTournages)
-// console.log(result);
-
-// // résultat : [1, 2, 7]
-
-
 // console.log(test)
 
+// autre manière de créer un filtre :
+//const elementsTournagesFiltre = elementsTournages.filter(tournage => tournage.fields.ardt_lieu == filtreArr); 
 
+
+
+//affichage type de tournages (tests initiaux)
+// console.log(tailleTableau) // stockage de la taille du tableau dans une variable utilisée pour définir la taille de la boucle
+// for (let i=0; i < tailleTableau; i++){
+//     // console.log('boucle n°'+i)
+//     let elementsParTournages = elementsTournages[i]
+//     let typeTournage = elementsParTournages.fields.type_tournage
+//     console.log(typeTournage) // récupération de la valeur de la clé type de tournage, stockée dans la clé field
+    
+//     const listeTypeTournage = document.createElement('p');
+//     listeTypeTournage.innerText = typeTournage
+//     titreListeTypeTournage.appendChild(listeTypeTournage)
+// }
